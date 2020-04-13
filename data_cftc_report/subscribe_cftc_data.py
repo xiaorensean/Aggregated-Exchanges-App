a9d0f91dff5068787b8c55a9f8ebe06147e80d86
xiaorensean <50184308+xiaorensean@users.noreply.github.com>
2020-04-13 14:09:34 -0400

    c

--- data_cftc_report/subscribe_cftc_data.py
+++ data_cftc_report/subscribe_cftc_data.py
@@ -38,17 +38,22 @@ def write_cftc_report(df_dict,host,measurement):
     
     for data in df_dict:
         # filter for tags and time
-        fields_raw = {dd:data[dd] for dd in data if dd != "CFTC_Contract_Market_Code" and dd != "Report_Date_as_MM_DD_YYYY"}
+        fields_raw = {dd:data[dd] for dd in data if \
+                      dd != "CFTC_Contract_Market_Code" and \
+                      dd != "Report_Date_as_MM_DD_YYYY"} #and \
+#                      dd != "Market_and_Exchanges_Names"}
         fields = fields_data_clean(fields_raw)
         tags = {} 
         tags.update({"CFTC_Contract_Market_Code":data["CFTC_Contract_Market_Code"]})
-        dbtime = False#df_dict[0]['Report_Date_as_MM_DD_YYYY']#datetime.datetime.strptime(str(df_dict[0]['Report_Date_as_MM_DD_YYYY']),"%Y-%m-%d %H:%M:%S")
+        #tags.update({"Market_and_Exchanges_Names":data["Market_and_Exchanges_Names"]})
+        dbtime = datetime.datetime.strptime(str(df_dict[0]['Report_Date_as_MM_DD_YYYY']),"%Y-%m-%d %H:%M:%S")
         host.write_points_to_measurement(measurement, dbtime, tags, fields)
 
 
 #all_file = list(sorted([i for i in os.listdir(current_dir+"/deafut") if i != ".DS_Store"]))
 #df = pd.read_excel(current_dir + "/deafut/" + all_file[-1])
 #code = list(set(df.CFTC_Contract_Market_Code.tolist()))
+#df_code = df[df.CFTC_Contract_Market_Code==code[1]]
 #df_dict_raw = df.T.to_dict()
 #df_dict = [df_dict_raw[i] for i in df_dict_raw]
 
