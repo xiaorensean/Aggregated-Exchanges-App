9fa8eebcff50dea999a6bf5e469ca98bb9052051
xiaorensean <50184308+xiaorensean@users.noreply.github.com>
2020-04-20 20:36:12 -0400

    c

--- influxdb_client/influxdb_client_host_1.py
+++ influxdb_client/influxdb_client_host_1.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__)))
 # host name 
 #HOST_1_old = 'ec2-35-183-41-80.ca-central-1.compute.amazonaws.com'
 #HOST_2_old = 'ec2-35-183-244-102.ca-central-1.compute.amazonaws.com'
-HOST_1 = "ec2-99-79-47-186.ca-central-1.compute.amazonaws.com"
+HOST_1 = "ec2-35-183-117-153.ca-central-1.compute.amazonaws.com"
 HOST_2 = "ec2-35-182-170-72.ca-central-1.compute.amazonaws.com"
 
 # database name
